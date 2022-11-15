3398aef90ff855a0d97c9aa7673e81a54dfbb282
ryanparketh <ryanpark@rocketmail.com>
2022-11-15 10:12:35 -0500

    added manager questions

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
+
+const groupMembers = [];
+
+const managerQuestions = [
+    {
+        type: 'input', 
+        name: 'name',
+        message: 'what is the managers name?',
+        validate: (input){
+            if (input !== ''){
+                return true;
+            }
+            return 'name has to have at least one character'
+        }
+    }
+]
+function buildManager(){
+    
+}
