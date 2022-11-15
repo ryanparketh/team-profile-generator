80d1985268d22f4d3ec1f8ff44859498df47cf0d
ryanparketh <ryanpark@rocketmail.com>
2022-11-15 09:54:08 -0500

    added const to index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
